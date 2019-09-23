c702612aeca001a0ed30f07113cc4ca2589da60a
bitgimo <tok991gimo@outlook.com>
2019-09-23 23:59:38 +0200

    Added runtime.txt and requirements.txt

--- ramen_ratings/settings.py
+++ ramen_ratings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y-j#1=trn$!a-^u%0!#++nk5c!k_84r99mx6c^q_id&t=8sopg'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
